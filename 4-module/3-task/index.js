b706787ba42267118183feba3c44da6685619fe0
Denis Tyukavin <dtyukavin@dmvdesk.com>
2021-07-08 18:31:58 +0300

    Resolved 4 module

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,30 @@
 function highlight(table) {
-  // ваш код...
+  let rows = table.rows;
+
+  for (let i = 1; i < rows.length; i++) {
+    const element = rows[i];
+    const ageCell = element.cells[1];
+    const genderCell = element.cells[2];
+    const statusCell = element.cells[3];
+    
+    if (statusCell.dataset.available === "true") {
+      element.classList.add("available");
+    } else {
+      element.classList.add("unavailable");
+    }
+
+    if (typeof statusCell.dataset.available === "undefined") {
+      element.setAttribute("hidden", "true");
+    }
+
+    if (genderCell.innerHTML === "f") {
+      element.classList.add("female");
+    } else {
+      element.classList.add("male");
+    }
+
+    if (ageCell.innerHTML < 18) {
+      element.style.textDecoration = "line-through";
+    }
+  }
 }
