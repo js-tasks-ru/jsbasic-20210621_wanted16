6a1ff879e947ad99a9b41c9417f8600e4541584f
Denis Tyukavin <dtyukavin@dmvdesk.com>
2021-07-20 23:44:33 +0300

    Resolved 5 module

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,36 @@
 function initCarousel() {
-  // ваш код...
+  const carouselInner = document.querySelector('.carousel__inner');
+  const carouselSlides = document.querySelectorAll('.carousel__slide');
+  
+  const carouselArrows = document.querySelectorAll('.carousel__arrow');
+  const carouselArrowLeft = document.querySelector('.carousel__arrow_left');
+  const carouselArrowRight = document.querySelector('.carousel__arrow_right');
+
+  let currentSlide = 0;
+
+  [...carouselArrows].forEach(element => {
+
+    element.addEventListener('click', event => {
+      let target = event.target;
+      let carouselArrow = target.closest('.carousel__arrow');
+
+      if ( carouselArrow.classList.contains('carousel__arrow_right') ) {
+        currentSlide++;
+      } else {
+        currentSlide--;
+      }
+
+      updateSlider(currentSlide);
+
+    });
+
+  });
+
+  function updateSlider(currentSlide) {
+    carouselInner.style.transform = `translateX(${-[...carouselSlides][currentSlide].offsetWidth * currentSlide}px)`;
+    currentSlide == carouselSlides.length - 1 ? carouselArrowRight.style.display = 'none' : carouselArrowRight.style.display = '';
+    currentSlide == 0 ? carouselArrowLeft.style.display = 'none' : carouselArrowLeft.style.display = '';
+  }
+
+  updateSlider(currentSlide);
 }
